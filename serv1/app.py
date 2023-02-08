3025c7d03d148f5f5baf86f05293a0a817562197
Girish Kumar DV <gkdv19@gmail.com>
2023-02-08 20:48:49 +0530

    init commit

---
+++ serv1/app.py
@@ -0,0 +1,43 @@
+from flask import Flask,redirect,url_for,render_template,request,flash
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/todo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+app.app_context().push()
+
+
+class Tasks(db.Model):
+    id = db.Column("id",db.Integer, primary_key=True)
+    title = db.Column("title",db.String(100))
+    stack = db.Column("stack",db.String(120))
+    mentors = db.Column("mentors",db.String(120))
+
+    def __init__(self,title,stack,mentors):
+        self.title = title
+        self.stack = stack
+        self.mentors = mentors
+
+
+@app.route("/",methods=["POST","GET"])
+def home():
+    if request.method == "POST":
+        title = request.form['title']
+        stack = request.form['stack']
+        mentor = request.form['mentors']
+        task = Tasks(title=title,stack=stack,mentors=mentor)
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for("viewTasks"))
+    else:
+        return render_template("index.html")
+ 
+@app.route("/tasks")
+def viewTasks():
+    tasks = Tasks.query.order_by(Tasks.id).all()
+    return render_template("tasks.html",content = tasks)
+
+
+if __name__=="__main__":
+    app.run(debug=True,port=4999)
---
+++ serv2/app.py
@@ -0,0 +1,47 @@
+from flask import Flask,redirect,url_for,render_template,request,flash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import declarative_base,relationship
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/todo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+app.app_context().push()
+
+class Users(db.Model):
+    id = db.Column("id",db.Integer, primary_key=True)
+    u_name = db.Column("u_name",db.String(100))
+    
+    def __init__(self,name):
+        self.u_name = name
+
+class Tasks(db.Model):
+    id = db.Column("id",db.Integer, primary_key=True)
+    title = db.Column("title",db.String(100))
+    stack = db.Column("stack",db.String(120))
+    mentors = db.Column("mentors",db.String(120))
+    def __init__(self,title,stack,mentors):
+        self.title = title
+        self.stack = stack
+        self.mentors = mentors
+
+@app.route("/",methods=['GET','POST'])
+def register():
+    if request.method == "POST":
+        uname = request.form['name']
+        user = Users(name=uname)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("assignedTasks",usr=user.id))
+    else:
+        return render_template('register.html')
+
+@app.route("/assignedTasks/<usr>")
+def assignedTasks(usr):
+    tasks = Tasks.query.order_by(Tasks.id).all()
+    user = Users.query.get(usr)
+    return render_template("tasks.html",content={'tasks':tasks,'user':user})
+
+
+if __name__=="__main__":
+    app.run(debug=True)
